397d0a2116b46807c42a0daa4ab6c4b67d90c6f9
Uzham <sathakhussam@gmail.com>
2020-08-08 21:02:41 +0530

    Upto Home,about,contact pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+    
+def about(request):
+    return render(request, 'pages/about.html')
+    
+def contact(request):
+    return render(request, 'pages/contact.html')
+    
+    
